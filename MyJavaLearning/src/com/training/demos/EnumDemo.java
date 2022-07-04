5f7ce7f939aaa2826584e504e580d57c2aab949b
Sukanta <sukanta.sahoo@jpmchase.com>
2022-07-04 16:32:44 +0530

    02 - Commit with a Change

--- MyJavaLearning/src/com/training/demos/EnumDemo.java
+++ MyJavaLearning/src/com/training/demos/EnumDemo.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class EnumDemo {
 	enum Fruits {
-		APPLE, ORANGE, LICHI, PINEAPPLE, MANGO
+		APPLE, ORANGE, LICHI, PINEAPPLE, MANGO, TESTFOOD
 	}
 
 	public static void main(String[] args) {
